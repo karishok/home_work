6d45bf0f5b1913035bcad7799b8b4dd7fbface15
kbabalyants <kbabalyants@ozon.ru>
2022-06-14 00:43:03 +0300

    HW02 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,30 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"unicode"
+	"unicode/utf8"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(input string) (output string, err error) {
+	for i, r := range input {
+		if unicode.IsDigit(r) {
+			if  i == 0 || unicode.IsDigit(rune(input[i-1])) {
+				return "", ErrInvalidString
+			}
+			n := int(r - '0')
+			for j := 1; j < n; j++ {
+				output += string(input[i-1])
+			}
+			if n == 0 {
+				length := utf8.RuneCountInString(output)
+				output = output[:length-1]
+			}
+		}
+		if unicode.IsLetter(r) {
+			output += string(input[i])
+		}
+	}
+	return output, nil
 }
